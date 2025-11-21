e02d48327b59bff00db02e5a5895d6ff0f945ef0
Antur <anturde21@gmail.com>
2025-11-21 17:17:09 +0600

    Added class.js

--- threedot.js
+++ threedot.js
@@ -9,7 +9,7 @@
 
 const ages = [12, 14, 16, 13, 17]
 const ages2 = [15, 16, 12]
-const allAges = [...ages, ...ages2]// concat means "join or combine things together"
+const allAges = [...ages, ...ages2] // combinig two array
 console.log(allAges);
 
 const number = [650, 450, 250]
